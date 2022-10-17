5618e7506fd9c8d0e43d349fb50cca7491fa4d55
mostafaabdlatef <mtgaafar97@gmail.com>
2022-10-17 19:41:42 +0200

    add cheout and about component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CheckoutComponent } from './Components/checkout/checkout.component';
+import { AboutComponent } from './Components/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CheckoutComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
