3735baefb70860b76549d241904a7181a2cbc626
LogyKahled <khaledlogyn@gmail.com>
2022-11-07 12:33:44 +0200

    signUp validationa & add phone and city inputs

--- src/app/Components/sign-up/sign-up.component.ts
+++ src/app/Components/sign-up/sign-up.component.ts
@@ -1,24 +1,30 @@
 import { Component } from '@angular/core';
-import { FormGroup,FormControl, Validators } from '@angular/forms';
+import { FormGroup,FormControl, Validators,FormBuilder } from '@angular/forms';
 import { PasswordValidators } from './password.validators';
+// import { FileUploadService } from './file-upload.service';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css']
 })
+
 export class SignUpComponent{
 
+  
  signUpForm = new FormGroup({
   firstName:new FormControl('', Validators.required),
   lastName:new FormControl('', Validators.required),
-  email:new FormControl('', Validators.required),
+  email:new FormControl('', [Validators.required,Validators.email]),
   password:new FormControl('', Validators.required),
   address:new FormControl('', Validators.required),
-  confirmPass:new FormControl('', Validators.required),
+  confirmPass:new FormControl('',Validators.required ),
+ phoneNumber:new FormControl('',[Validators.required,Validators.pattern('^01[0125][0-9]{8}$')]),
+
 },
 [PasswordValidators.MatchValidator('password','confirmPass')]
 );
+
 get FirstName(){
   return this.signUpForm.get('firstName')
 }
@@ -38,6 +44,9 @@ get passwordMatchError() {
     this.signUpForm.getError('mismatch') &&
     this.signUpForm.get('confirmPass')?.touched
   );
+}
+get phoneNumber(){
+  return this.signUpForm.get('phoneNumber')
 }
 
   
