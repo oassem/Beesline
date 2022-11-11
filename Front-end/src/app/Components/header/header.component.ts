cb6e2b1671b5bfbdef54c7899ff6a3e38db5cca4
oassem <noneed762@gmail.com>
2022-11-12 00:09:28 +0200

    Modified header

--- Front-end/src/app/Components/header/header.component.ts
+++ Front-end/src/app/Components/header/header.component.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit, SimpleChanges, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { SignInComponent } from '../sign-in/sign-in.component';
 import { MatMenuTrigger } from '@angular/material/menu';
-import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -12,14 +11,16 @@ export class HeaderComponent implements OnInit {
   searchValue: any;
   userName: string = '';
 
-  constructor(private dialog: MatDialog, private router: Router) {
+  constructor(private dialog: MatDialog) {
     let token = localStorage.getItem('token');
     if (token != null) {
       let data = localStorage.getItem('userData');
       let userData = JSON.parse(data!);
       let userObj: any = Object.values(userData)['0'];
       this.userName = userObj['firstname'];
-    } else this.userName = 'SignIn';
+    } else {
+      this.userName = 'SignIn';
+    }
   }
   ngOnInit(): void {}
 
