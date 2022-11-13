a993adec33d60fe5248ef2d8eacf1d5c05c81301
mostafaabdlatef <mtgaafar97@gmail.com>
2022-11-13 23:00:34 +0200

    Graduation Project Branch

--- Back-end/app/Http/Controllers/OrderController.php
+++ Back-end/app/Http/Controllers/OrderController.php
@@ -10,23 +10,27 @@ use App\Models\User_Order;
 
 class OrderController extends Controller
 {
+
     public function store(StoreandUpdateOrderRequest $request)
     {
         Order::Create($request->all());
-        $user_order = new User_Order();
-        $user_order->user_id = $request->user_id;
-        $user_order->order_id = $request->id;
-        $user_order->save();
+        $temp = User_Order::where('order_id', $request->id)->first();
+        if (!$temp) {
+            $user_order = new User_Order();
+            $user_order->user_id = $request->user_id;
+            $user_order->order_id = $request->id;
+            $user_order->save();
+        }
     }
-   
+
     public function _orders(){
         $orders= Order::distinct()->get(['id','user_id','orderStatus']);
         return OrderResource::collection($orders);
     }
-    
+
     public function index()
     {
-      
+
         $orders = Order::all();
         return OrderResource::collection($orders);
     }
@@ -34,7 +38,7 @@ class OrderController extends Controller
     public function show($id)
     {
         $orders = Order::where('id', $id)->get();
-      
+
         if ($orders) {
             return OrderResource::collection($orders);
         } else {
@@ -44,8 +48,8 @@ class OrderController extends Controller
 
     public function update($id,StoreandUpdateOrderRequest $request)
     {
-       
-        Order::find($id)->update($request->all()); 
+
+        Order::find($id)->update($request->all());
     }
 
     public function destroy($id)
